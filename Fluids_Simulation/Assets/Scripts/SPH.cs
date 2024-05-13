ad59985f8845618e9584cdfdef91fcff9295577e
ytyonly <daguguguji18@gmail.com>
2024-05-13 13:16:44 +0200

    particles

---
+++ Fluids_Simulation/Assets/Scripts/SPH.cs
@@ -0,0 +1,137 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System.Linq;
+
+public class SPH : MonoBehaviour
+{
+    [System.Serializable]
+    //[StructLayout(LayoutKind.Sequential, Size = 44)]
+    public struct Particle
+    {
+        public float pressure;
+        public float density;
+        public Vector3 currentForce;
+        public Vector3 velocity;
+        public Vector3 position;
+    }
+
+    [Header("General")]
+    public bool showSphere = true;
+    public Vector3Int numToSpawn = new Vector3Int(10, 10, 10);
+    private int totalParticles
+    {
+        get
+        {
+            return numToSpawn.x * numToSpawn.y * numToSpawn.z;
+        }
+    }
+    public Vector3 boxSize = new Vector3(4, 10, 3);
+    public Vector3 spawnCenter;
+    public float particleRadius = 0.1f;
+
+    [Header("Particle Rendering")]
+    public Mesh particleMesh;
+    public float particleRenderSize = 8f;
+    public Material material;
+
+    [Header("Compute")]
+    public ComputeShader shader;
+    public Particle[] particles;
+
+    private ComputeBuffer _argsBuffer;
+    private ComputeBuffer _particlesBuffer;
+
+    private static readonly int SizeProperty = Shader.PropertyToID("_size");
+    private static readonly int ParticlesBufferProperty = Shader.PropertyToID("_particlesBuffer");
+
+    private void Awake()
+    {
+        SpawnParticlesInBox();
+
+        uint[] args =
+        {
+            particleMesh.GetIndexCount(0),
+            (uint)totalParticles,
+            particleMesh.GetIndexStart(0),
+            particleMesh.GetBaseVertex(0),
+            0
+        };
+        _argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        _argsBuffer.SetData(args);
+
+        _particlesBuffer = new ComputeBuffer(totalParticles, 44);
+        Debug.Log(particles.Count());
+
+        _particlesBuffer.SetData(particles);
+    }
+
+    private void SpawnParticlesInBox()
+    {
+        Vector3 spawnPoint = spawnCenter;
+        List<Particle> _particles = new List<Particle>();
+
+        for (int x = 0; x < numToSpawn.x; x++)
+        {
+            for (int y = 0; y < numToSpawn.y; y++)
+            {
+                for (int z = 0; z < numToSpawn.z; z++)
+                {
+
+                    Vector3 spawnPosition = spawnPoint + new Vector3(x * particleRadius * 2, y * particleRadius * 2, z * particleRadius * 2);
+                    Debug.Log(spawnPosition);
+
+                    Particle p = new Particle
+                    {
+                        position = spawnPosition
+                    };
+
+                    _particles.Add(p);
+
+                }
+            }
+        }
+        Debug.Log(_particles.Count);
+        particles = _particles.ToArray();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireCube(Vector3.zero, boxSize);
+
+        if (!Application.isPlaying)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireCube(spawnCenter, new Vector3(0.1f, 0.1f, 0.1f));
+        }
+    }
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.SetFloat(SizeProperty, particleRenderSize);
+        material.SetBuffer(ParticlesBufferProperty, _particlesBuffer);
+        //Debug.Log(_particlesBuffer);
+
+        if (showSphere)
+        {
+            Graphics.DrawMeshInstancedIndirect(
+                particleMesh,
+                0,
+                material,
+                new Bounds(Vector3.zero, boxSize),
+                _argsBuffer,
+                castShadows: UnityEngine.Rendering.ShadowCastingMode.Off
+        );
+        }
+
+    }
+}
